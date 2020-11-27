f871b2d89d9ed47f87b4dcde4dd8200bc0ff20bc
Akaed <43893798+Akaed@users.noreply.github.com>
2020-11-27 17:14:48 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+
+static const int SIZE = 8;
+
+class Vector {
+protected:
+    int buffer[SIZE];
+
+public:
+    Vector() {
+        for (int i = 0; i < SIZE; i++) {
+            buffer[i] = 0;
+        }
+    }
+    int operator   [](int i) const    {return buffer[i];}
+    int & operator [](int i)          {return buffer[i];}
+};
+
+int main() {
+    Vector v;
+    int value = v[0];
+    int & const_value = v[7];
+
+    value = 256;
+    const_value = 256;
+
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d ", v[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
